afca3b26137a45e24d301160017c15603779299a
rifkyrizkita <rifky.rizkita.g@gmail.com>
2023-06-25 19:38:33 +0700

    test

--- src/components/article.jsx
+++ src/components/article.jsx
@@ -1,83 +1,76 @@
-import { Tabs, TabList, TabPanels, Tab, TabPanel, Table, Tbody, Tr, Td, Box, Flex, Image, } from '@chakra-ui/react';
+import { Box, Flex, Tab, TabList, TabPanel, TabPanels, Tabs, Image, Text, Heading, Button } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
-import Axios from "axios"
+import Axios from "axios";
 import { useNavigate } from 'react-router-dom';
 
 export const Article = () => {
-  const [data, setdata] = useState({})
-  const navigate = useNavigate()
+  const [data, setData] = useState({});
+  const navigate = useNavigate();
+
   const handleClick = (id) => {
     navigate(`blog/${id}`);
   };
 
-useEffect(() => {
-  Axios.get("https://minpro-blog.purwadhikabootcamp.com/api/blog/pagFav?page=1&orderBy=total_fav&sort=DESC&size=10").then((res) => {
-    console.log(res);
-    setdata(res.data)
-  })
-}, [])
+  useEffect(() => {
+    Axios.get("https://minpro-blog.purwadhikabootcamp.com/api/blog/pagFav?page=1&orderBy=total_fav&sort=DESC&size=10")
+      .then((res) => {
+        console.log(res);
+        setData(res.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
   return (
-    <Box   >
-      <Flex justify={"center"}  >
-        
-    <Tabs variant="soft-rounded" bgColor={"blue.400"} maxW={"80vw"} color={"white"} align={"center"} >
-      <TabList >
-        <Tab>Popular</Tab>
-       
-      </TabList>
-      <TabPanels >
-        <TabPanel>
-          <Table >
-            <Tbody  overflowX={"auto"}>
-              <Tr border={"2px" }  > 
-                {data.result ? data.result.map((value, index) => {
-                  return(
-                    
-
-                    <Td  key={index} i> {value.title}</Td>
-                    
-
-                  )
-                }): null}
-              </Tr>
-              <Tr >
-                {data.result ? data.result.map((value, index) => {
-                  return(
-                    
-
-                    <Td key={index}> {`Likes : ${value.total_fav}`}</Td>
-                    
-
-                  )
-                }): null}
-              </Tr>
-              <Tr >
-                {data.result ? data.result.map((value, index) => {
-                  return(
-                    
-
-                    <Td key={index} > 
-                    <Flex justifyContent={"center"} w={"100px"}>
-
-                    <Image cursor={"pointer"} border={"1px"} w={"100px"} h={"100px"} borderRadius={"50%"} objectFit={"cover"} src={`https://minpro-blog.purwadhikabootcamp.com/${value.imageURL}`} onClick={() => handleClick(value.id)}/>
-                    </Flex>
-                    </Td>
-                    
-
-                  )
-                }): null}
-              </Tr>
-              
-            </Tbody>
-          </Table>
-        </TabPanel>
-       
-      </TabPanels>
-    </Tabs>
-      </Flex>
+    <Box w="100%" p={4} bg="blue.400">
+      <Tabs variant="soft-rounded" colorScheme="blue" align="center">
+        <TabList>
+          <Tab _selected={{ color: 'blue.400', bg: 'white' }}>Popular</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <Flex wrap="wrap" justify="center" align="center" rowGap={4} columnGap={4}>
+              {data.result &&
+                data.result.map((value, index) => (
+                  <Box
+                    key={index}
+                    w="300px"
+                    borderWidth="1px"
+                    borderRadius="lg"
+                    overflow="hidden"
+                    bg="white"
+                    boxShadow="md"
+                    transition="transform 0.3s"
+                    _hover={{ transform: 'scale(1.02)' }}
+                  >
+                    <Image src={`https://minpro-blog.purwadhikabootcamp.com/${value.imageURL}`} alt={value.title} boxSize="300px" objectFit="cover" />
+                    <Box p={4}>
+                      <Heading size="md" mb={2} isTruncated maxW="100%" fontWeight="semibold" fontSize="lg">
+                        {value.title}
+                      </Heading>
+                      <Text color="gray.600" fontSize="sm" overflowWrap="break-word">
+                        {`Likes: ${value.total_fav}`}
+                      </Text>
+                      <Box mt={4}>
+                        <Button
+                          colorScheme="blue"
+                          onClick={() => handleClick(value.id)}
+                          size="sm"
+                          fontWeight="bold"
+                          _hover={{ opacity: 0.8 }}
+                          overflowWrap="break-word"
+                        >
+                          Read More
+                        </Button>
+                      </Box>
+                    </Box>
+                  </Box>
+                ))}
+            </Flex>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </Box>
   );
 };
-
-
